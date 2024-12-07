1fee6f9b4a21a54f959362c569a4fd9359666a11
rezguisarra <rezguisarra@gmail.com>
2024-12-07 09:50:46 +0000

    rrtt

---
+++ riadh/src/main/java/tn/gov/sibtel/riadh/RiadhApplication.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.riadh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiadhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiadhApplication.class, args);
+	}
+
+}
---
+++ riadh/src/test/java/tn/gov/sibtel/riadh/RiadhApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.riadh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiadhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
